cfda840a272392bcd030c8cf939b97a734de8a48
Alfonso Saavedra Son <sonlink.dourden@gmail.com>
2013-08-28 22:19:37 +0200

    r3: Cambios en la traducción"

--- fantasynamesgenerator.py
+++ fantasynamesgenerator.py
@@ -20,21 +20,15 @@ gettext.bindtextdomain (APP, './locale')
 _ = gettext.gettext
 gettext.install(APP, './locale', unicode=1)
 
-loc = locale.getlocale()[0]
-if loc.find('es') == -1:
-	# Si el idioma del sistema no es el español en cualquiera de sus variantes, se coge el ingles por defecto
-	lang = gettext.translation(APP, './locale', languages=['en'])
-	lang.install()
-
 class GUI():
 	def __init__(self):
 		# Comprobamos antes de nada que los 2 directorios existen y que tienen permisos de ejecucion
-		if not access('txt', R_OK):
-			self.error_dialog(_('No tienes permisos de lectura en las carpeta txt.\nCompruebe los permisos'))
+		if not access('names', R_OK):
+			self.error_dialog(_("You don't have read permisions on names directory"))
 			sys.exit(1)
 		else:
 			mainwin = gtk.Window()
-			mainwin.set_title(_(u'Generador de nombres fantásticos'))
+			mainwin.set_title('Fantasy Names Generator')
 			mainwin.set_position(gtk.WIN_POS_CENTER_ALWAYS)
 			mainwin.set_default_size(400,300)
 
@@ -42,7 +36,7 @@ class GUI():
 
 			vbox = gtk.VBox()
 
-			lang_label = gtk.Label(str=_('Elija la raza:'))
+			lang_label = gtk.Label(str=_('Select race:'))
 			lang_label.set_alignment(0, 0)
 			vbox.pack_start(lang_label, False, False, 0)
 
@@ -51,19 +45,19 @@ class GUI():
 			cell = gtk.CellRendererText()
 			self.lang_select.pack_start(cell, True)
 			self.lang_select.add_attribute(cell, 'text', 1)
-			self.liststore.append(['drow', _('Drow/Elfo Oscuro'), True])
-			self.liststore.append(['elf', _('Elfo'), False])
+			self.liststore.append(['drow', _('Drow/Dark elf'), True])
+			self.liststore.append(['elf', _('Elf'), False])
 			self.liststore.append(['hafling', 'Hafling', True])
-			self.liststore.append(['dwarven', _('Enanos'), True])
-			self.liststore.append(['gnome', _('Gnomos'), True])
-			self.liststore.append(['demons', _('Demonios'), False])
-			self.liststore.append(['dragons', _(u'Dragones'), False])
-			self.liststore.append(['orcs', _('Orcos'), False])
+			self.liststore.append(['dwarven', _('Dwarven'), True])
+			self.liststore.append(['gnome', _('Gnomes'), True])
+			self.liststore.append(['demons', _('Demons'), False])
+			self.liststore.append(['dragons', _(u'Dragons'), False])
+			self.liststore.append(['orcs', _('Orcs'), False])
 			self.lang_select.connect('changed', self.check_combobox)
 
 			vbox.pack_start(self.lang_select, False, False, 0)
 
-			sex_label = gtk.Label(str=_('Elija el sexo:'))
+			sex_label = gtk.Label(str=_('Select sex:'))
 			sex_label.set_alignment(0, 0)
 			vbox.pack_start(sex_label, False, False, 0)
 
@@ -72,13 +66,13 @@ class GUI():
 			cell2 = gtk.CellRendererText()
 			self.sex_select.pack_start(cell2, True)
 			self.sex_select.add_attribute(cell2, 'text', 1)
-			liststore2.append(['male', _('Masculino')])
-			liststore2.append(['female', _('Femenino')])
+			liststore2.append(['male', _('Male')])
+			liststore2.append(['female', _('Female')])
 			vbox.pack_start(self.sex_select, False, False, 0)
 			self.sex_select.connect('changed', self.check_combobox)
 			self.sex_select.set_sensitive(False)
 
-			total_label = gtk.Label(str=_('Total a generar (1-100):'))
+			total_label = gtk.Label(str=_('Total generating (1-100):'))
 			sex_label.set_alignment(0, 0)
 			vbox.pack_start(total_label, False, False, 0)
 
@@ -87,17 +81,17 @@ class GUI():
 			self.total_value.set_increments(1.0, 5.0)
 			vbox.pack_start(self.total_value, False, False, 0)
 
-			self.generate = gtk.Button(label=_('Generar listado'))
+			self.generate = gtk.Button(label=_('Generate list'))
 			self.generate.connect('clicked', self.generar)
 			self.generate.set_sensitive(False)
 			vbox.pack_start(self.generate, False, False, 0)
 
-			self.save = gtk.Button(label=_('Guardar'))
+			self.save = gtk.Button(label=_('Save'))
 			self.save.set_sensitive(False)
 			self.save.connect('clicked', self.save_on_file)
 			vbox.pack_start(self.save, False, False, 0)
 
-			info = gtk.Button(label=_('Sobre'))
+			info = gtk.Button(label=_('About'))
 			info.connect('clicked', self.view_info)
 			vbox.pack_start(info, False, False, 0)
 
@@ -134,9 +128,9 @@ class GUI():
 
 	def save_on_file(self, w):
 		try:
-			select_files = gtk.FileChooserDialog(title=_('Seleccione donde se guardaran los nombres'),action=gtk.FILE_CHOOSER_ACTION_SAVE, buttons=(gtk.STOCK_SAVE,gtk.RESPONSE_OK, gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL))
+			select_files = gtk.FileChooserDialog(title=_('Select where to save names'),action=gtk.FILE_CHOOSER_ACTION_SAVE, buttons=(gtk.STOCK_SAVE,gtk.RESPONSE_OK, gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL))
 			filter = gtk.FileFilter()
-			filter.set_name(_("Texto"))
+			filter.set_name(_("Text"))
 			filter.add_mime_type("text/plain")
 			filter2 = gtk.FileFilter()
 			filter2.set_name("CSV")
@@ -150,21 +144,21 @@ class GUI():
 			if response == gtk.RESPONSE_OK:
 				select_files.hide()
 				if path.exists(select_files.get_filenames()[0]):
-					warning = gtk.MessageDialog(parent=self.mainwin, flags=0, type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_YES_NO, message_format=_("¡Atención!"))
-					warning.format_secondary_text(_(u'El archivo ya existe.\n¿Desea sobreescribirlo?'))
+					warning = gtk.MessageDialog(parent=self.mainwin, flags=0, type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_YES_NO, message_format=_("Attention!"))
+					warning.format_secondary_text(_('The file already exists.\nReplace?'))
 					def close(w, res):
 						if res == gtk.RESPONSE_NO:
 							w.hide()
 						elif res == gtk.RESPONSE_YES:
 							w.hide()
-							if mime_name == _("Texto"):
+							if mime_name == _("Text"):
 								self.to_text(select_files.get_filenames()[0])
 							elif mime_name == 'CSV':
 								self.to_csv(select_files.get_filenames()[0])
 
 					warning.connect("response", close)
 					warning.run()
-				if mime_name == _("Texto"):
+				if mime_name == _("Text"):
 					self.to_text(select_files.get_filenames()[0])
 				elif mime_name == 'CSV':
 					self.to_csv(select_files.get_filenames()[0])
@@ -172,7 +166,7 @@ class GUI():
 			else:
 				select_files.hide()
 		except IOError:
-			self.error_dialog(_('Ocurrió un error al guardar el archivo.\nCompruebe que tienes permisos de escritura en el directorio'))
+			self.error_dialog(_('An error occurred while saving the file.\nMake sure you have write permissions on the directory.'))
 
 	def to_txt(self, filename):
 		if not filename.endswith('.txt'):
@@ -201,21 +195,21 @@ class GUI():
 	def view_info(self, widget):
 		# esta funcion se limita a mostrar y cerrar el dialogo de la info
 		info = gtk.AboutDialog()
-		info.set_name(_('Generador de nombres fantásticos'))
+		info.set_name('Fantastic Names Generator')
 		info.set_version('1 beta 2')
 		f = open('COPYING', 'r')
 		info.set_license(f.read())
-		info.set_comments(_(u"Genera nombres fantásticos de varias razas"))
+		info.set_comments(_("Generate names of many fantasy races"))
 		info.set_website('http://sonlinkblog.blogspot.com')
-		info.set_translator_credits(_('Ingles: Son Link'))
-		info.set_website_label(_("Pagina del proyecto"))
+		info.set_translator_credits(_('English: Son Link'))
+		info.set_website_label(_("proyect's webpage"))
 		def close(w, res):
 			w.hide()
 		info.connect("response", close)
 		info.run()
 
 	def error_dialog(self, message):
-		warning = gtk.MessageDialog(parent=None, flags=0, type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_OK, message_format=_("¡Error!"))
+		warning = gtk.MessageDialog(parent=None, flags=0, type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_OK, message_format=_("Error!"))
 		warning.format_secondary_text(message)
 		def close(w, res):
 			w.hide()
